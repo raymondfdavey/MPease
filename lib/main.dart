fbd8b574af21f18c2650311919643cebde34625d
dan-colyer <dancolyer@icloud.com>
2020-10-24 16:46:34 +0100

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-Future fetchAlbum() async {
+final String baseUrl =
+    'http://eldaddp.azurewebsites.net/lordsregisteredinterests.json';
 
+Future<List<Lord>> fetchLords(int page) async {
+  var response = await http.get(baseUrl); // + page.toString() when needed
+  List<Lord> listOfLords = [];
+
+  if (response.statusCode == 200) {
+    final dynamic result = response.body;
+    List decodedList = jsonDecode(result)['result']['items'];
+
+    for (int i = 0; i <= decodedList.length - 1; i++) {
+      Lord newLord = new Lord();
+      newLord.name = decodedList[i]['fullName']['_value'];
+      listOfLords.add(newLord);
+    }
+  } else {
+    throw Exception('Failed to load lords');
+  }
+
+  print(listOfLords[0].name);
+  print(listOfLords[1].name);
+  return listOfLords;
+}
+
+Future fetchAlbum() async {
   final response = await http
       .get('http://eldaddp.azurewebsites.net/lordsregisteredinterests.json');
 
   if (response.statusCode == 200) {
-
+    List<Lord> newListOfLords = new List<Lord>();
     List<Map<String, dynamic>> listOfLords = [];
     Map<String, dynamic> results = {};
+    Map<String, dynamic> results2 = {};
     var lordsSummary = jsonDecode(response.body);
     num noOfPages = lordsSummary['result']['totalResults'] / 500;
     noOfPages = noOfPages.ceil();
+
     for (int i = 0; i <= noOfPages - 1; i++) {
       String resultsKey = 'page $i';
-      results[resultsKey] = await http.get('http://eldaddp.azurewebsites.net/lordsregisteredinterests.json?_pageSize=500&_page=$i');
+      results[resultsKey] = await http.get(
+          'http://eldaddp.azurewebsites.net/lordsregisteredinterests.json?_pageSize=500&_page=$i');
       print('done page $i');
       if (results[resultsKey].statusCode == 200) {
         continue;
@@ -24,20 +51,29 @@ Future fetchAlbum() async {
         throw Exception('Failed to load lords');
       }
     }
+
     results.forEach((k, v) => results[k] = jsonDecode(v.body));
-    results.forEach((k,v) => results[k]['result']['items'].forEach((lord) => listOfLords.add(lord)));
-    listOfLords.forEach((lord) => print(lord['fullName']['_value']));
+
+    results.forEach((k, v) =>
+        results[k]['result']['items'].forEach((lord) => listOfLords.add(lord)));
+
+    for (int i = 0; i <= listOfLords.length - 1; i++) {
+      Lord newLord = new Lord();
+      newLord.name = listOfLords[i]['fullName']['_value'];
+      newListOfLords.add(newLord);
+    }
+
+    print(newListOfLords[0].name);
+    print(newListOfLords[1].name);
+    print(newListOfLords);
+
     //FAVE LORD???? MINE IS BARONESS SHACKLETON OF BELGRAVIA THOUGH THERE ARE A FEW. I THINK YOUR SPIRIT LORD IS VISCOUNT YOUNGER OF LECKIE OR MAYBE LORD PICKLES
   }
 }
 
-
-
-
-
-
-
-
+class Lord {
+  String name;
+}
 
 void main() {
   runApp(MyApp());
@@ -50,11 +86,13 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   Future futureAlbum;
+  Future lords;
 
   @override
   void initState() {
     super.initState();
-    futureAlbum = fetchAlbum();
+    // futureAlbum = fetchAlbum();
+    lords = fetchLords(0);
   }
 
   @override
@@ -68,21 +106,23 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('Fetch Data Example'),
         ),
-        // body: Center(
-        //   child: FutureBuilder<LordsAlbum>(
-        //     future: futureAlbum,
-        //     builder: (context, snapshot) {
-        //       if (snapshot.hasData) {
-        //         return Text(snapshot.data.name);
-        //       } else if (snapshot.hasError) {
-        //         return Text("${snapshot.error}");
-        //       }
-        //
-        //       // By default, show a loading spinner.
-        //       return CircularProgressIndicator();
-        //     },
-        //   ),
-        // ),
+        body: Center(
+          child: FutureBuilder<List<Lord>>(
+            future: lords,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data[0].name);
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              print(snapshot);
+
+              // By default, show a loading spinner.
+              return CircularProgressIndicator();
+            },
+          ),
+        ),
       ),
     );
   }
