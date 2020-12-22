63991b20d330db8073df8af151a222ec1fef39b8
dan-colyer <dancolyer@icloud.com>
2020-12-22 15:24:22 +0100

    wip

--- lib/classes.dart
+++ lib/classes.dart
@@ -1,30 +1,14 @@
-class Lord {
-  String id;
-  String firstName;
-  String middleName;
-  String surname;
-  String title;
-  String gender;
-  String dob;
-  String dobFormatted;
-  String age;
-  String party;
-  String peerageType;
-  String beganLording;
-  String beganLordingFormatted;
-  String beganLordingInYears;
-  String isActive;
-  List<Interest> interests;
-}
-
 class LordBaseList {
-  List<LordListedViewModel> member;
+  List<Lord> member;
 }
 
-class LordListedViewModel {
+class Lord {
   int memberId;
   String displayName;
-  // bool isActive;
+  String memberFrom;
+  String party;
+  DateTime dob;
+  List<Interest> interests;
 }
 
 // Dan do:
